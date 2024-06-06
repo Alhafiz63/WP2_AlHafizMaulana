f32cde132b38f634492c23a22a5d973c9ef3e5ff
Alhafiz63 <hafizbanjar.jan05@gmail.com>
2024-06-06 13:50:01 +0800

    iniMenu

--- project00/project00.cpp
+++ project00/project00.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+void dMenu();
+
 int main() {
-
-    std::cout << "hello world";
-
+    std::cout << "hello world" << std::endl;
+    dMenu();  // Call dMenu function
     return 0;
+}
+
+void dMenu() {
+    system("cls");
+    std::cout << "Aplikasi Template Menu" << "\n";
+    std::cout << "1. Menu pertama" << "\n";
+    std::cout << "2. Menu kedua" << "\n";
+    std::cout << "3. Menu ketiga" << "\n";
+    std::cout << "4. Menu keempat" << "\n";
+    std::cout << "5. Exit" << "\n";
+    std::cout << "Masukkan angka :";
 }
